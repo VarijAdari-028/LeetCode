90fb6b263edff567b148ad7c0de68188bd2d8ae7
Varij Adari <varijadari1028@gmail.com>
2026-09-07 23:28:37 +0530

    Time: 11 ms (61.98%), Space: 19.3 MB (46.35%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        MOD = 10**9 + 7
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        last = [0] * 26  # last[c] = 1-indexed position of last occurrence, 0 if none
+
+        for i in range(1, n + 1):
+            c = ord(s[i - 1]) - ord('a')
+            dp[i] = (2 * dp[i - 1]) % MOD
+            if last[c] != 0:
+                dp[i] = (dp[i] - dp[last[c] - 1]) % MOD
+            last[c] = i
+
+        return (dp[n] - 1) % MOD
