902d6fa0c55f935fcafea2426092b28b13d2ad33
Varij Adari <varijadari1028@gmail.com>
2026-09-14 18:39:39 +0530

    Time: 6 ms (42.41%), Space: 20 MB (23.21%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        used = [False] * len(nums)
+
+        def backtrack(current):
+            if len(current) == len(nums):
+                result.append(current[:])
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                # skip duplicates: only use the first unused instance of equal values
+                # at this position to avoid generating the same permutation twice
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+
+                used[i] = True
+                current.append(nums[i])
+                backtrack(current)
+                current.pop()
+                used[i] = False
+
+        backtrack([])
+        return result
